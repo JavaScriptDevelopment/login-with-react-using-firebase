48b689259e6020b093ae303ce9b55b42dad0d0af
A.Rupesh <a.rupesh66@gmail.com>
2018-08-19 21:31:38 +0530

    Update Authen.js

--- src/Authen.js
+++ src/Authen.js
@@ -1,4 +1,21 @@
 import React, { Component } from 'react';
+var firebase = require(firebase);
+
+var config = {
+    apiKey: "AIzaSyAgO_Ma0UO_rxCoJmKMeknFc4tCbPlI8m4",
+    authDomain: "usurvey-73d0d.firebaseapp.com",
+    databaseURL: "https://usurvey-73d0d.firebaseio.com",
+    projectId: "usurvey-73d0d",
+    storageBucket: "usurvey-73d0d.appspot.com",
+    messagingSenderId: "48941761363"
+  };
+  firebase.initializeApp(config);
+
+
+
+
+
+
 
 class Authen extends Component{
     render(){
